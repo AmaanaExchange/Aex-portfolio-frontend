e060a6282dafc2f8204d90bd26c69698c2f60f0f
Abdul Ahad <abdulahadbinkhalid@gmail.com>
2023-09-27 19:24:43 +0500

    Added coming soon page and made improvements for responsive design

---
+++ src/components/sections/Dialog.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Dialog = () => {
+  return (
+    <div>
+      <div className="alert">
+        <div className="d-flex">
+          <span className="linear-heading"> Sorry we’re still building </span>
+          <h1 className="icon">🛠️</h1>
+        </div>
+        <div className="my-3 d-flex">
+          <span className="linear-heading"> Come back in a bit </span>{" "}
+          <h1 className="icon">🤍️</h1>
+        </div>
+        <div >
+          <span className="linear-heading">
+            {" "}
+            Because we’re changing the way things are done!{" "}
+          </span>
+          <h1 className="icon">🚀</h1>
+        </div>
+
+        <div className="mt-5 mb-5">
+          <b className="linear-text"> Sincerely,</b>
+        </div>
+        <div>
+          <span className="linear-text"> Amaana Exchange Team</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dialog;
--- src/components/sections/GetStarted.js
+++ src/components/sections/GetStarted.js
@@ -6,46 +6,48 @@ import IconGetCrypto from "../../assets/images/icon-get-crypto.svg";
 
 export const GetStarted = () => {
   return (
+  
     <section className="get-started">
-      <Container>
-        <Row className="align-items-center">
-          <Col lg={12} className="text-center">
-            <h1 className="section-title mx-auto">Get Started In Just a Few Minutes</h1>
-            <p className="section-sub">We make buying, trading and selling safe and secure </p>
+  <Container>
+    <Row className="align-items-center">
+          <Col lg={12} className="text-center " style={{ position: "relative" }}>
+        <h1 className="section-title mx-auto">Get Started In Just a Few Minutes</h1>
+        <p className="section-sub">We make buying, trading, and selling safe and secure</p>
+      </Col>
+      <Col lg={10} className="mx-auto">
+        <Row>
+          <Col md={6} lg={4}>
+            <Card className="mb-4">
+              <Link to="#" className="d-flex flex-column align-items-center">
+                <Image src={IconRegister} alt="Register" />
+                <h4>Register</h4>
+                <p>Buy Bitcoin or Ethereum, then securely store it or send it to friends</p>
+              </Link>
+            </Card>
           </Col>
-          <Col lg={11} className="mx-auto">
-            <Row>
-              <Col md={4}>
-                <Card>
-                  <Link to="#">
-                    <Image src={IconRegister} />
-                    <h4>Register</h4>
-                    <p>Buy Bitcoin or Ethereum, the securely store it in your wallet or send it on easily to friends </p>
-                  </Link>
-                </Card>
-              </Col>
-              <Col md={4}>
-                <Card>
-                  <Link to="#">
-                    <Image src={IconDeposit} />
-                    <h4>Deposit</h4>
-                    <p>Choose you preferred payment method such as bank transfer or credit card to top up your wallet </p>
-                  </Link>
-                </Card>
-              </Col>
-              <Col md={4}>
-                <Card>
-                  <Link to="#">
-                    <Image src={IconGetCrypto} />
-                    <h4>Get Crypto</h4>
-                    <p>Sign up for free wallet on web, IOS or Android and follow our easy process to set up your profile</p>
-                  </Link>
-                </Card>
-              </Col>
-            </Row>
+          <Col md={6} lg={4}>
+            <Card className="mb-4">
+              <Link to="#" className="d-flex flex-column align-items-center">
+                <Image src={IconDeposit} alt="Deposit" />
+                <h4>Deposit</h4>
+                <p>Top up your wallet using your preferred payment method, like bank transfer or credit card</p>
+              </Link>
+            </Card>
+          </Col>
+          <Col md={6} lg={4}>
+            <Card className="mb-4" style={{ position: "relative", zIndex: "1" }}>
+              <Link to="#" className="d-flex flex-column align-items-center">
+                <Image src={IconGetCrypto} alt="Get Crypto" />
+                <h4>Get Crypto</h4>
+                <p>Sign up for a free wallet on the web, iOS, or Android and follow our easy profile setup process</p>
+              </Link>
+            </Card>
           </Col>
         </Row>
-      </Container>
-    </section>
+      </Col>
+    </Row>
+  </Container>
+</section>
+
   );
 };
